b2614354b5127d07f728a2b105b048a7a6776cdc
MrAndreID <andreaadam215@gmail.com>
2021-02-01 00:31:21 +0700

    add func. for verify signature;

--- gosignature.go
+++ gosignature.go
@@ -15,8 +15,16 @@ type Signer interface {
 	Sign(data []byte) ([]byte, error)
 }
 
+type Unsigner interface {
+	Unsign(data []byte, signature []byte) error
+}
+
 type rsaPrivateKey struct {
 	*rsa.PrivateKey
+}
+
+type rsaPublicKey struct {
+	*rsa.PublicKey
 }
 
 func (rsaPrivateKey *rsaPrivateKey) Sign(data []byte) ([]byte, error) {
@@ -26,6 +34,15 @@ func (rsaPrivateKey *rsaPrivateKey) Sign(data []byte) ([]byte, error) {
 	hashed := hash.Sum(nil)
 
 	return rsa.SignPKCS1v15(rand.Reader, rsaPrivateKey.PrivateKey, crypto.SHA256, hashed)
+}
+
+func (rsaPublicKey *rsaPublicKey) Unsign(data []byte, signature []byte) error {
+	hash := sha256.New()
+	hash.Write(data)
+
+	hashed := hash.Sum(nil)
+
+	return rsa.VerifyPKCS1v15(rsaPublicKey.PublicKey, crypto.SHA256, hashed, signature)
 }
 
 func Generate(privateKey []byte, value []byte) (string, error) {
@@ -42,6 +59,26 @@ func Generate(privateKey []byte, value []byte) (string, error) {
 	}
 
 	return base64.StdEncoding.EncodeToString(signed), nil
+}
+
+func Verify(signature string, publicKey []byte, value []byte) (bool, error) {
+	parser, err := parsePublicKey(publicKey)
+
+	if err != nil {
+		return false, errors.New("failed to parse the public key.")
+	}
+
+	key, err := base64.StdEncoding.DecodeString(signature)
+
+	if err != nil {
+		return false, errors.New("invalid signature.")
+	}
+	
+	if parser.Unsign(value, key) != nil {
+		return false, errors.New("could not sign the request.")
+	}
+
+	return true, nil
 }
 
 func parsePrivateKey(privateKey []byte) (Signer, error) {
@@ -76,7 +113,45 @@ func newSignerFromPrivateKey(rawPrivateKey interface{}) (Signer, error) {
 	case *rsa.PrivateKey:
 		sshKey = &rsaPrivateKey{privateKeyType}
 	default:
+		return nil, errors.New("key type not supported [4042].")
+	}
+
+	return sshKey, nil
+}
+
+func parsePublicKey(publicKey []byte) (Unsigner, error) {
+	block, _ := pem.Decode(publicKey)
+
+	if block == nil {
+		return nil, errors.New("no key was found.")
+	}
+
+	var rawPublicKey interface{}
+
+	switch block.Type {
+	case "PUBLIC KEY":
+		rsa, err := x509.ParsePKIXPublicKey(block.Bytes)
+
+		if err != nil {
+			return nil, err
+		}
+
+		rawPublicKey = rsa
+	default:
 		return nil, errors.New("key type not supported [4041].")
+	}
+
+	return newUnsignerFromPublicKey(rawPublicKey)
+}
+
+func newUnsignerFromPublicKey(rawPublicKey interface{}) (Unsigner, error) {
+	var sshKey Unsigner
+
+	switch publicKeyType := rawPublicKey.(type) {
+	case *rsa.PublicKey:
+		sshKey = &rsaPublicKey{publicKeyType}
+	default:
+		return nil, errors.New("key type not supported [4042].")
 	}
 
 	return sshKey, nil
